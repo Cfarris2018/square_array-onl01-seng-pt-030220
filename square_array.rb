6dbee04c7790345d1093e0e5b136b6a1fe446060
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-12 03:22:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 
 def square_array(array)
   my_array = []
-  array.each do |n|
-   my_array << n ** 2
-    my_array << square
+  array.each do |n| square = n ** 2
+    my_array << square}
   end
   my_array
 end
